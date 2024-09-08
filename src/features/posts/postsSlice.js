af51296a02b587dad78e2d390588b26280a6135c
Adrian Cretu <adrian.cretu03@gmail.com>
2024-09-08 19:12:06 +0300

    feat: add form for new post and new date field

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
@@ -6,12 +7,14 @@ const initialState = [
     title: "The Importance of Teamwork",
     content:
       "In sports, individual talent shines, but teamwork wins championships.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: "2",
     title: "Pre-Game Routines",
     content:
       "A solid pre-game routine can be the difference between victory and defeat.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
   },
 ];
 
@@ -21,7 +24,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.posts.push(action.payload);
+        state.push(action.payload);
       },
       prepare(title, content) {
         return {
@@ -29,6 +32,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
           },
         };
       },
